dba848b33dce027472205755ea48035856496606
Aayush <aayushbastola772@gmail.com>
2025-07-27 22:34:52 +0545

    api for view

--- src/main/java/bastolaaayush/com/np/billing/controller/ViewController.java
+++ src/main/java/bastolaaayush/com/np/billing/controller/ViewController.java
@@ -1,4 +1,31 @@
 package bastolaaayush.com.np.billing.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+
 public class ViewController {
+
+    @GetMapping("/home")
+    public String showMainPage(){
+        return "home";
+    }
+    @GetMapping("/about")
+    public String aboutPage(){
+        return "about";
+    }
+    @GetMapping("/calculateBill")
+    public String calculateBillPage(){
+        return "calculateBill";
+    }
+
+    @GetMapping("/bills")
+    public String bills(){
+        return "bills";
+    }
+
+
 }
